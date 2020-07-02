58515d5a154417bfeb1a1499cf12b07bd68760bf
Giovanni Lion <giovanni.lion@gmail.com>
2020-07-02 07:30:18 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in almavivens/__init__.py
